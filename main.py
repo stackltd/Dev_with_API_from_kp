42fce48ca8a585f08a73d692ec27a5a4234ee746
Evgeny Fedorov <stackltd@yandex.ru>
2023-08-28 21:52:59 +0700

    adding functionality and some fixes

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from messages import *
 
 parse = Parser()
 
-
 @timer
 def main() -> None:
     """
@@ -30,28 +29,51 @@ def main() -> None:
             else:
                 print('База данных не была пополнена')
         elif ask_1 == '2':
-            id_mov = input('Введите требуемый id\n')
+            id_movies = []
+            id_mov = input('Введите название, по которому будет произовдиться поиск.\n'
+                           'Если нужно найти фильм по известному id, нашите звездочку после него\n')
             ask_2 = input(ask_print)
-            parse.make_json(id_movie=id_mov, prnt=ask_2=='1')
+            if id_mov.endswith('*'):
+                id_movies.append(id_mov[:-1])
+            else:
+                id_movies.extend(parse.list_id(id_mov))
+            parse.make_json(id_movies=tuple(id_movies), prnt=ask_2=='1')
         elif ask_1 == '3':
             ask_2 = input('Будет выведена таблица с краткой информацией о фильмах.'
                           ' Если же хотите вывести все данные, введите 1, если только таблицу - любой символ\n')
             res = parse.get_json(prnt=ask_2 == '1')
             parse.print_table(obj=res)
             while True:
-                ask_3 = input('Введите для сортировки по возрастанию: 1: по имени, 2: по году, 3: по типу, 4: по голосам, 5: по стране. '
-                              'По убыванию - введите двойное число'
-                              '\nИли введите id для поиска фильма. Выход в основное меню - нажмите энтер.\n')
-                if ask_3 in ('1', '2', '3', '4', '5', '11', '22', '33', '44', '55'):
-                    field_sort = ask_3
+                ask_3 = input('Введите для сортировки по возрастанию: 1: по имени, 2: по году, 3: по типу, 4: по жанру,'
+                              ' 5: по голосам, 6: по стране. '
+                              'В обратном порядке - введите двойное число'
+                              '\nДля поиска фильма и вывода в адаптированном виде введите id. В json-формате - напишите звездочку после id.'
+                              '\nДля удаления фильма из базы введите его id и напишите знак минус (-) после него'
+                              '\nВыход в основное меню - нажмите энтер.\n')
+                if ask_3 in ('1', '2', '3', '4', '5', '6', '11', '22', '33', '44', '55', '66'):
+                    field_sort = ask_3 if len(ask_3) == 1 else ask_3[:1]
                     is_reverse = len(ask_3) == 2
                     parse.print_table(obj=res, field_sort=field_sort, is_reverse=is_reverse)
                 elif ask_3 != '':
-                    parse.get_json(prnt=True, id_movie=ask_3)
-                    input('Нажмите ввод для вывода таблицы ')
+                    if ask_3.endswith('*'):
+                        parse.get_json(prnt=True, id_movie=ask_3[:-1])
+                        input('Нажмите ввод для вывода таблицы ')
+                    elif ask_3.endswith('-'):
+                        parse.del_movie(ask_3[:-1])
+                        res = parse.get_json()
+                        parse.print_table(obj=res)
+                    else:
+                        obj = parse.get_json(prnt=False, id_movie=ask_3)
+                        parse.print_info(obj=obj)
+                        input('Нажмите ввод для вывода таблицы ')
                     parse.print_table(obj=res)
                 else:
                     break
+        elif ask_1 == '4':
+            ask_2 = input('Введите id фильма, который нужно удалить\n')
+            parse.del_movie(ask_2)
+            res = parse.get_json()
+            parse.print_table(obj=res)
         else:
             break
 
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import re
 
 from prettytable import PrettyTable
 
@@ -12,9 +13,21 @@ RANDOM_MOVIE = '/v1/movie/random'
 INFO = '/v1.3/movie/'
 ALL_SEAS_EPIS = '/v1/season'
 REVIEW = '/v1/review'
-FIELDS = {'1': 'name', '2': 'year', '3': 'type', '4': 'votes', '5': 'countries',
-          '11': 'name', '22': 'year', '33': 'type', '44': 'votes', '55': 'countries'}
 
+FIELDS = {'1': 'name', '2': 'year', '3': 'type', '4': 'genres', '5': 'votes', '6': 'countries'}
+
+PATT1 = r"""[^<>a-zA-Z;\|_\{\}\[\]\\"=!@%&^*()\+]+"""
+PATT2 = r"""/+[0-9]+/+"""
+PATT3 = r"""&#\d+|&?#\d+"""
+PATT4 = r"""[^/]"""
+PATT5 = r"""\.,"""
+PATTERNS = [PATT1, PATT2, PATT3, PATT4, PATT5]
+
+KEYS_SEARCHE = {'name': 'название', 'description': 'описание', 'type': 'тип', 'year': 'год', 'releaseYears': 'период',
+                'genres': 'жанр', 'status': 'статус', 'ageRating': 'ограничение по возрасту', 'facts': 'факты о фильмы',
+                'countries': 'страны', 'rating': 'рейтинг', 'votes': 'голоса', 'backdrop': 'фон', 'poster': 'постер',
+                'seasonsInfo': 'сезоны', 'seriesLength': 'серий', 'totalSeriesLength': 'всего серий',
+                'similarMovies': 'похожие фильмы', 'sequelsAndPrequels': 'сериалы и приквелы', 'persons': 'актеры и прочие'}
 
 class Parser:
     """
@@ -48,32 +61,35 @@ class Parser:
         else:
             return res
 
-    def make_json(self, id_movie='', prnt=False) -> None:
+    def make_json(self, id_movies=('',), prnt=False) -> None:
         """
         Метод обновляет json-файл результатом парсинга API с разными эндпоинтами.
-        :param id_movie: параметр, разрешающий вывод результата в консоль.
-        :param prnt: ключ id, по которому производится поиск если необходимо.
+        :param prnt: параметр, разрешающий вывод результата в консоль.
+        :param id_movie: кортеж с id, по которым производится поиск, если необходимо.
         """
         # Создаем объект из дампа.
         with open('dumps/movies_info.json', 'r', encoding='utf-8') as file:
             dump_in: dict = json.load(file)
         # Получаем json объект из стороннего API.
-        obj = self.parse_json(endpoint=INFO, id_movie=id_movie, prnt=prnt) if id_movie else self.parse_json(endpoint=RANDOM_MOVIE,
-                                                                                                   prnt=prnt)
-        # Проверка соответствия типа полученного объекта obj и наличия в объекте ключа id.
-        if isinstance(obj, dict) and (id_movie := obj.get('id', 0)):
-            id_movie = str(id_movie)
-            isSeries = obj['isSeries']
-            dump_temp = dict.fromkeys(['Общая информация о фильме'], obj)
-            dump_temp.update(
-                {'Информация о сезонах и эпизодах': self.parse_json(endpoint=ALL_SEAS_EPIS, id_movie=id_movie, prnt=prnt) if isSeries else {}})
-            dump_temp.update({'Отзывы зрителей': self.parse_json(endpoint=REVIEW, id_movie=id_movie, prnt=prnt)})
-            dump_in.update({id_movie: dump_temp})
-            with open(f'dumps/movies_info.json', 'w', encoding='utf-8') as file:
-                json.dump(dump_in, file, indent=4, ensure_ascii=False)
-            self.count += 1
-        else:
-            print('По этому id ничего не найдено!')
+        for id_movie in id_movies:
+            if id_movie in dump_in.keys():
+                continue
+            obj = self.parse_json(endpoint=INFO, id_movie=id_movie, prnt=prnt) if id_movie\
+                else self.parse_json(endpoint=RANDOM_MOVIE, prnt=prnt)
+            # Проверка соответствия типа полученного объекта obj и наличия в объекте ключа id.
+            if isinstance(obj, dict) and (id_movie := obj.get('id', 0)):
+                id_movie = str(id_movie)
+                isSeries = obj['isSeries']
+                dump_temp = dict.fromkeys(['Общая информация о фильме'], obj)
+                dump_temp.update(
+                    {'Информация о сезонах и эпизодах': self.parse_json(endpoint=ALL_SEAS_EPIS, id_movie=id_movie, prnt=prnt) if isSeries else {}})
+                dump_temp.update({'Отзывы зрителей': self.parse_json(endpoint=REVIEW, id_movie=id_movie, prnt=prnt)})
+                dump_in.update({id_movie: dump_temp})
+                with open(f'dumps/movies_info.json', 'w', encoding='utf-8') as file:
+                    json.dump(dump_in, file, indent=4, ensure_ascii=False)
+                self.count += 1
+            else:
+                print('По этому id ничего не найдено!')
 
     @staticmethod
     def get_json(prnt=False, id_movie=''):
@@ -95,24 +111,144 @@ class Parser:
         Метод формирует поля для вывода информации в табличном варианте.
         """
         # Сортировка словаря по значениям полей
-
         if field_sort:
             obj = {x: obj[x] for x in sorted(obj.keys(), key=lambda a:
-            int(obj[a][text].get(FIELDS[field_sort]).get('kp', 0)) if field_sort in ('4', '44') else str(
+            int(obj[a][text].get(FIELDS[field_sort]).get('kp', 0)) if field_sort in ('5', '55') else str(
                 obj[a][text].get(FIELDS[field_sort], '')), reverse=is_reverse
                                              )}
         # В данном списке формируются поля для таблицы. В случае длинных текстов, они обрезаются.
         list_mov = [[ind + 1,
                      key,
-                     list(map(lambda x: x[:25] + '...' if len(x) > 25 else x, [str(obj[key][text]['name'])]))[0],
+                     list(map(lambda x: x[:25] + '..' if len(x) > 25 else x, [str(obj[key][text]['name'])]))[0],
                      obj[key][text]['year'],
-                     obj[key][text]['type'],
+                     obj[key][text]['type'][:12],
+                     ','.join(list(map(lambda x: x['name'][:3] if len(obj[key][text]['genres']) >= 3 else x['name'],
+                                       obj[key][text]['genres']))),
                      obj[key][text]['votes'].get('kp', '-'),
-                     list(map(lambda x: x[:75] + ' (...)' if len(x) > 75 else x,
-                              [str(obj[key][text].get('description', ''))]))[0],
-                     ', '.join(list(map(lambda x: x['name'] if x else '', obj[key][text]['countries'])))]
+                     list(map(lambda x: x[:70] + '..' if len(x) > 75 else x,
+                              [str(obj[key][text].get('description', ''))]))[0].replace('\n', ''),
+                     ','.join(list(map(lambda x: x['name'] if len(obj[key][text]['countries']) == 1 else x['name'][:3],
+                                       obj[key][text]['countries'])))[:28]]
                     for ind, key in enumerate(obj.keys())]
         my_table = PrettyTable()
-        my_table.field_names = ["id", "mov_id", "name", "year", "голосов", "рейтинг", "описание", "страна"]
+        my_table.field_names = ["id", "mov_id", "name", "year", "тип", "жанр", "голосов", "описание", "страна"]
         my_table.add_rows(list_mov)
         print(my_table)
+
+
+    @staticmethod
+    def print_info(obj):
+        info_movie = obj.get('Общая информация о фильме')
+        info_seasons = obj.get('Информация о сезонах и эпизодах')
+        info_preview = obj.get('Отзывы зрителей')
+        if not info_movie:
+            print('Нет данных по этому id')
+            return
+
+        def filter_text(patterns, text):
+            def parse_html(text, pattern, replace=False, repl=''):
+                res = re.findall(pattern, text)
+                if replace:
+                    text_out = re.sub(pattern, repl, text)
+                else:
+                    text_out = ' '.join(''.join(res).split())
+                return ' '.join(''.join(text_out).split())
+
+            for ind, patt in enumerate(patterns):
+                replace = ind in (1, 2, 4)
+                if ind == 4:
+                    repl = '.'
+                else:
+                    repl = ''
+                text = parse_html(text=text, pattern=patt, replace=replace, repl=repl)
+            return text
+
+        def previews_info():
+            print('\n' + '-' * 10 + 'Отзывы' + '-' * 10)
+            if info_preview:
+                obj = info_preview['docs']
+                for preview in obj:
+                    print(f"Ник: {preview.get('author')}, вывод: {preview.get('title')}, оценка: {preview.get('type')}")
+                    text = filter_text(patterns=PATTERNS, text=preview['review'])
+                    print(text)
+                    print('=' * 200 + '\n')
+
+        def seasons_info():
+            print('\n' + '-' * 10 + 'Информация о сезонах' + '-' * 10)
+            if info_seasons:
+                obj = info_seasons['docs']
+                for seasons in obj[::-1]:
+                    print(f"Сезон {seasons['number']}, число серий: {seasons.get('episodesCount')}")
+                    string = '\n    '.join(
+                        [f"{x['number']}.{x['name']} ({x['enName']}). {x['description']}" for x in seasons['episodes']])
+                    print(string)
+
+        def movie_info():
+            print('-' * 10 + 'Общая информация' + '-' * 10)
+
+            # sequelsAndPrequels
+
+            for i in KEYS_SEARCHE:
+                try:
+                    if info_movie.get(i, {}):
+                        if i == 'rating':
+                            string = info_movie[i]['kp']
+                        elif i == 'votes':
+                            string = info_movie[i]['kp']
+                        elif i in ('poster', 'backdrop'):
+                            string = info_movie[i]['url']
+                        elif i in ('facts', 'genres', 'countries') and info_movie[i] is not None:
+                            if i == 'facts':
+                                key = 'value'
+                            else:
+                                key = 'name'
+                            string = ', '.join([x[key] for x in info_movie[i]])
+                            string = filter_text(patterns=PATTERNS, text=string)
+                        elif i == 'seasonsInfo':
+                            string = ', '.join(
+                                [f"номер: {x['number']} (серий: {x['episodesCount']})" for x in info_movie[i]])
+                        elif i == 'releaseYears':
+                            string = ', '.join([f"начало: {x['start']}, конец: {x['end']}" for x in info_movie[i]])
+                        elif i == 'persons':
+                            string = '\n    '.join(
+                                [f"{ind + 1}. {x['profession'][:-1]}: {x['enName']} ({x['name']}) {x['photo']}" for
+                                 ind, x in enumerate(info_movie[i])])
+                        elif i == 'similarMovies':
+                            string = '\n    '.join(
+                                [f"{x['name']} ({x['enName']}{x['alternativeName']}). id={x['id']} {x.get('poster').get('url')}" for x in info_movie[i]])
+                        elif i == 'description':
+                            string = filter_text(patterns=PATTERNS, text=info_movie[i])
+                        elif i == 'sequelsAndPrequels':
+                            string = ',\n    '.join([f"{x['name']} ({x['alternativeName']}), {x['type']}, id: {x['id']} {x.get('poster').get('url')}" for x in info_movie[i]])
+                        else:
+                            string = info_movie[i]
+                        print(f"{KEYS_SEARCHE[i]}:\n    {string}")
+                except Exception as ex:
+                    print(f'{ex=}')
+                    raise
+
+        movie_info()
+        input('Нажмите ввод для вывода информации о сезонах')
+        print()
+        seasons_info()
+        input('Нажмите ввод для вывода отзывов')
+        print()
+        previews_info()
+
+    @staticmethod
+    def list_id(name):
+        url = f'https://api.kinopoisk.dev/v1.3/movie?name={name}'
+        res: dict = requests.get(url=url, headers=HEADERS).json()
+        res_out = res['docs']
+        list_out = [str(x['id']) for x in res_out]
+        print(f'Найдено {len(res_out)} фильмов')
+        return list_out
+
+
+    @staticmethod
+    def del_movie(movie_id):
+        with open('dumps/movies_info.json', 'r', encoding='utf-8') as file:
+            dump_in: dict = json.load(file)
+        dump_in.pop(movie_id, {})
+        with open(f'dumps/movies_info.json', 'w', encoding='utf-8') as file:
+            json.dump(dump_in, file, indent=4, ensure_ascii=False)
